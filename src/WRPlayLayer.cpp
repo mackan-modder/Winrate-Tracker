ea21b1a4c9cf2d40229de86142e5a9f9449f7143
Mackan-Modder <ultimata444@gmail.com>
2026-05-29 10:04:05 +0200

    Added PlayerObject and finished basic run tracking. Implemented updates of the winrate.

--- src/WRPlayLayer.cpp
+++ src/WRPlayLayer.cpp
@@ -14,16 +14,15 @@ class $modify(WRPlayLayer, PlayLayer){
 	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
 		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
 
+        geode::log::info("init()");
+
 		for(int i=0;i<100;i++){
 			m_fields->percentageWinrate[i] = 1;
 		}
 
-		
-		
 		return true;
 	}
 
-	
 	void levelComplete() {
 		updateWinrate(m_fields->startingPercentage,100);
 		
@@ -31,14 +30,13 @@ class $modify(WRPlayLayer, PlayLayer){
 	}
 	
 	void resetLevel() {	
-		updateWinrate(m_fields->startingPercentage,getCurrentPercentInt());
-		
 		PlayLayer::resetLevel();
 
 		m_fields->startingPercentage = getCurrentPercentInt();
 	}
 
 	void updateWinrate(int start, int end) {
+        geode::log::info("updateWinrate()");
 		for(int i=start;i<end;i++){
             geode::log::info("index: {} old: {} new: {}",i,m_fields->percentageWinrate[i],ALPHA + m_fields->percentageWinrate[i]*(1-ALPHA));
 			m_fields->percentageWinrate[i] = ALPHA + m_fields->percentageWinrate[i]*(1-ALPHA);
---
+++ src/WRPlayerObject.cpp
@@ -0,0 +1,17 @@
+#include <Geode/modify/PlayerObject.hpp>
+#include "WRPlayLayer.cpp"
+
+class $modify(WRPlayerObject, PlayerObject){
+
+    void playerDestroyed(bool noEffects) {
+        if (!PlayLayer::get()) return;
+
+        geode::log::info("playerDestroyed()");
+
+        auto pl = static_cast<WRPlayLayer*>(PlayLayer::get()); // Special thanks to Jump Markers by TechStudent10
+
+        pl->updateWinrate(pl->m_fields->startingPercentage,pl->getCurrentPercentInt());
+
+        PlayerObject::playerDestroyed(noEffects);
+    }
+};
