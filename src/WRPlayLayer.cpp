454be5c0afb4977fbe260e1c9a7f862cfc280c0d
Mackan-Modder <ultimata444@gmail.com>
2026-05-29 09:44:48 +0200

    first commit

---
+++ src/WRPlayLayer.cpp
@@ -0,0 +1,61 @@
+
+#include <Geode/modify/PlayLayer.hpp>
+
+const float ALPHA = 0.1;
+
+using namespace geode::prelude;
+
+class $modify(WRPlayLayer, PlayLayer){
+	struct Fields {
+		float percentageWinrate[100];
+		float startingPercentage;
+	};
+
+	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+
+		for(int i=0;i<100;i++){
+			m_fields->percentageWinrate[i] = 1;
+		}
+
+		
+		
+		return true;
+	}
+
+	
+	void levelComplete() {
+		updateWinrate(m_fields->startingPercentage,100);
+		
+		PlayLayer::levelComplete();
+	}
+	
+	void resetLevel() {	
+		updateWinrate(m_fields->startingPercentage,getCurrentPercentInt());
+		
+		PlayLayer::resetLevel();
+
+		m_fields->startingPercentage = getCurrentPercentInt();
+
+		// geode::log::info("------------------------------");
+
+		// geode::log::info("------------------------------");
+		// for(int i=0;i<100;i++){
+		// 	geode::log::info("{}",m_fields->percentageWinrate[i]);
+		// }
+		// geode::log::info("------------------------------");
+	}
+
+	void updateWinrate(int start, int end) {
+		for(int i=start;i<end;i++){
+            geode::log::info("index: {} old: {} new: {}",i,m_fields->percentageWinrate[i],ALPHA + m_fields->percentageWinrate[i]*(1-ALPHA));
+			m_fields->percentageWinrate[i] = ALPHA + m_fields->percentageWinrate[i]*(1-ALPHA);
+		}
+
+		
+		if (end<100) {
+            geode::log::info("index: {} old: {} new: {}",end,m_fields->percentageWinrate[end],m_fields->percentageWinrate[end]*(1-ALPHA));
+			m_fields->percentageWinrate[end] = m_fields->percentageWinrate[end]*(1-ALPHA);
+		}
+	}
+};
